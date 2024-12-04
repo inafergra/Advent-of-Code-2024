d2d940ead8d83e6f52446dcc93e2394d18242311
Ignacio <ignacio.fernandez-grana@pasqal.com>
2024-12-04 09:48:34 +0100

    day1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -4,9 +4,36 @@ use std::fs::read_to_string;
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+
+    let filename: &str= "input/input_1.txt";
+    let mut column1: Vec<i32> = Vec::new();
+    let mut column2: Vec<i32> = Vec::new();
+    for line in read_to_string(filename).unwrap().lines() {
+        let l :Vec<&str>= line.split_whitespace().collect();
+        let word1: i32 = l[0].parse::<i32>().unwrap();
+        let word2: i32 = l[1].parse::<i32>().unwrap();
+        column1.push(word1);
+        column2.push(word2);
+    }
+    column1.sort();
+    column2.sort();
+
+
+    // Part 1
+    let mut sol1: i32 = 0;
+    for (i,j) in column1.clone().into_iter().zip(column2.clone().into_iter()) {
+        sol1 += (i-j).abs();
+    };
+
+    // Part 2
+    let mut sol2: i32 = 0;
+    for i in &column1 {
+        for j in &column2 {
+            if j==i {
+                sol2 += i
+            }
+        }
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
 }
