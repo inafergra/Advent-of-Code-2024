33005e559ef90437efbb06a96e855ee39e85ef70
Ignacio <ignacio.fernandez-grana@pasqal.com>
2024-12-11 00:31:40 +0100

    formatting with rustfm

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -4,12 +4,11 @@ use std::fs::read_to_string;
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-
-    let filename: &str= "input/input_1.txt";
+    let filename: &str = "input/input_1.txt";
     let mut column1: Vec<i32> = Vec::new();
     let mut column2: Vec<i32> = Vec::new();
     for line in read_to_string(filename).unwrap().lines() {
-        let l :Vec<&str>= line.split_whitespace().collect();
+        let l: Vec<&str> = line.split_whitespace().collect();
         let word1: i32 = l[0].parse::<i32>().unwrap();
         let word2: i32 = l[1].parse::<i32>().unwrap();
         column1.push(word1);
@@ -18,18 +17,17 @@ pub fn solve() -> SolutionPair {
     column1.sort();
     column2.sort();
 
-
     // Part 1
     let mut sol1: i32 = 0;
-    for (i,j) in column1.clone().into_iter().zip(column2.clone().into_iter()) {
-        sol1 += (i-j).abs();
-    };
+    for (i, j) in column1.clone().into_iter().zip(column2.clone().into_iter()) {
+        sol1 += (i - j).abs();
+    }
 
     // Part 2
     let mut sol2: i32 = 0;
     for i in &column1 {
         for j in &column2 {
-            if j==i {
+            if j == i {
                 sol2 += i
             }
         }
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,42 +1,55 @@
 use crate::{Solution, SolutionPair};
-use std::fs::{read_to_string, File};
 use itertools::Itertools;
+use std::fs::{read_to_string, File};
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-
-    fn is_safe(report:&Vec<i16>)->bool {
+    fn is_safe(report: &Vec<i16>) -> bool {
         let mut i0: &i16 = &report[0];
         let direction = &report[1] - i0;
-        let mut safe: bool = true;        
+        let mut safe: bool = true;
         for i in &report[1..] {
-            let diff = i-i0;
-            if diff.abs() <1 {safe = false}
-            else if diff.abs()>3 {safe=false}
-            else if direction*diff <0 {safe = false}
-            
-            if safe==false {break}
+            let diff = i - i0;
+            if diff.abs() < 1 {
+                safe = false
+            } else if diff.abs() > 3 {
+                safe = false
+            } else if direction * diff < 0 {
+                safe = false
+            }
+
+            if safe == false {
+                break;
+            }
             i0 = i;
         }
         safe
     }
 
-
-    let filename: &str= "input/input_2.txt";
+    let filename: &str = "input/input_2.txt";
     let mut sol1: i16 = 0;
     for line in read_to_string(filename).unwrap().lines() {
-        let report :Vec<i16>= line.split_whitespace().map(|s| s.parse::<i16>().unwrap()).collect();
+        let report: Vec<i16> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i16>().unwrap())
+            .collect();
 
-        if is_safe(&report) {sol1 +=1; continue }
+        if is_safe(&report) {
+            sol1 += 1;
+            continue;
+        }
         println!("{}", "Report:");
         println!("{:?}", report);
 
-        let combs = report.iter().combinations(report.len()-1);
+        let combs = report.iter().combinations(report.len() - 1);
         for comb in combs {
             let new_report: Vec<i16> = comb.iter().map(|&&x| x).collect();
             println!("{:?}", new_report);
-            if is_safe(&new_report) {sol1 +=1; break}
+            if is_safe(&new_report) {
+                sol1 += 1;
+                break;
+            }
         }
     }
 
